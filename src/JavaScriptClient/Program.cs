d215d4b056b6e1be2f2589591a9e3169a14b6225
Robert Folkesson <robert.folkesson@activesolution.se>
2023-09-20 22:07:39 +0200

    Added BFF config

--- src/JavaScriptClient/Program.cs
+++ src/JavaScriptClient/Program.cs
@@ -1,4 +1,35 @@
+using System.IdentityModel.Tokens.Jwt;
+using Duende.Bff.Yarp;
+
 var builder = WebApplication.CreateBuilder(args);
+
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllers();
+
+builder.Services.AddBff().AddRemoteApis();
+
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = "Cookies";
+        options.DefaultChallengeScheme = "oidc";
+        options.DefaultSignOutScheme = "oidc";
+    })
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.ClientId = "bff";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+        options.Scope.Add("api1");
+        options.SaveTokens = true;
+        options.GetClaimsFromUserInfoEndpoint = true;
+    });
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -10,6 +41,24 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseBff();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapBffManagementEndpoints();
+
+    endpoints.MapControllers()
+        .AsBffApiEndpoint();
+
+    endpoints.MapRemoteBffApiEndpoint("/remote", "https://localhost:6001")
+        .RequireAccessToken(Duende.Bff.TokenType.User);
+});
+
 
 app.Run();
 
